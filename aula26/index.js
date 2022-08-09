351d1c800e4a6fa9f56b00bd8cde5cd0f674f7d6
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-08-09 16:09:17 -0300

    revisão numeros

--- aula26/index.js
+++ aula26/index.js
@@ -1,10 +1,28 @@
-let num1 = 10.3355    //  number
-let num2 = 2.5  //  number
+let num1 = 0.7  //  number
+let num2 = 0.1  //  number
 
-//  console.log(num1.toString() + num2) **passando number para string temporario**
-//  num1 = num1.toString() **convertendo number em string na raiz**
-//  console.log(num1.toString(2)) **convertendo para binario**
-//  console.log(num1.toFixed(2)) **definindo 2 casas decimais**
-//  console.log(Number.isInteger(num1)) **teste se number é inteiro**
+// COMANDOS EM JAVASCRIPT //
+
+//  console.log(num1.toString() + num2)     **passando number para string temporario**
+//  num1 = num1.toString()                  **convertendo number em string na raiz**
+//  console.log(num1.toString(2))           **convertendo para binario**
+//  console.log(num1.toFixed(2))            **definindo 2 casas decimais**
+//  console.log(Number.isInteger(num1))     **teste se number é inteiro**
     /*let temp = num1 * 'hello'
-    console.log(Number.isNaN(temp)) teste para saber se é NaN*/
+    console.log(Number.isNaN(temp))         teste para saber se é NaN*/
+
+// CONTAS COM PONTO FLUTUANTE //
+
+    num1 += num2
+    num1 += num2
+    num1 += num2
+
+    num1 = Number(num1.toFixed(2))
+
+    console.log(num1)
+    console.log(Number.isInteger(num1))
+
+    
+
+
+
