90215f0232d1e6b2cbc57045fffbf85981c17cba
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-08-10 07:19:44 -0300

    metodo math

---
+++ aula27/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.54
+
+//  let num2 = Math.floor(num1) **arredonda para baixo
+//  let num2 = Math.ceil(num1)  **arredonda para cima
+//  let num2 = Math.round(num1) **arredonda baixo ou cima de 0,5
+//  console.log(Math.max(2,5,8,7,9))    **retorna maior valor
+//  console.log(Math.min(2,5,8,7,9))    **retorna menor valor
+//  const aleatorio = Math.round(Math.random() * (10 - 5) + 5)  **retorna numero aleatorio
+console.log(aleatorio)
