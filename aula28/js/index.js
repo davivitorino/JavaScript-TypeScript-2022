923df71117b3174313086a91b64b4cee2cea6edf
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-08-11 16:34:00 -0300

    exercicio atualizado

--- aula28/js/index.js
+++ aula28/js/index.js
@@ -1,4 +1,16 @@
 const valor = Number(window.prompt("digite um numero"))
 let saida = document.getElementById('numero')
+let resultado = document.getElementById('resultado')
 
 saida.innerHTML = `${valor}`
+resultado.innerHTML += `<p>raiz quadrada de ${valor} é = ${Math.sqrt(valor)}</P>`
+resultado.innerHTML += `<p>o numero ${valor} é inteiro? = ${Number.isInteger(valor)}</p>`
+resultado.innerHTML += `<p>é NaN? = ${isNaN(valor)}</p>`
+resultado.innerHTML += `<p>arredondamento para baixo = ${Math.floor(valor)}</p>`
+resultado.innerHTML += `<p>arredondamento para cima = ${Math.ceil(valor)}</p>`
+resultado.innerHTML += `<p>com duas casas decimais = ${valor.toFixed(2)}</p>`
+
+
+
+
+
