40ee697b6048012e15ddd6c9b818ea7d9343f05c
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-08-12 17:01:39 -0300

    array basico

---
+++ aula29/index.js
@@ -0,0 +1,12 @@
+// Array Basic Methods
+
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+console.log( numeros instanceof Array ) // testa se objeto é array
+/*
+numeros.unshift(0)  // adicionando no inicio do array
+numeros.shift() // remove primeiro elemento
+numeros.push(11)    // adiciona na última posição do array
+numeros.pop()   //remove última posição do array
+*/
+console.log(numeros.slice(0, -5))
