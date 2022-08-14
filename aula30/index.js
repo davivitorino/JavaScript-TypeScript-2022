ddb83dec3699f0e7d573e1c00cefa55ddff3b67c
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-08-14 02:27:13 -0300

    aula sobre funcao

---
+++ aula30/index.js
@@ -0,0 +1,34 @@
+// function basic
+
+/**
+
+function saudacao(nome) {
+    return `Bom Dia ${nome}!`           
+}
+                                   
+const variavel = saudacao('davi')
+console.log(variavel)
+
+
+function soma(x, y) {
+    const resultado = x + y
+    return resultado
+}
+
+const valor1 = console.log(soma(4,8))
+const valor2 = console.log(soma(3,7))
+
+
+const soma = function(x, y) {
+    const resultado = x + y
+    return resultado
+}
+
+console.log(soma(7, 3))
+
+**/
+
+const soma = (x, y) => x + y
+
+console.log(soma(7, 3))
+
