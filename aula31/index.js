f8883aa02175469fdea0f45be1188540eac7465b
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-08-18 14:43:16 -0300

    objeto atualizado

--- aula31/index.js
+++ aula31/index.js
@@ -1,12 +1,34 @@
-// objetc basic
+/**objetc basic**/
+
+/*função para criar objeto
 
 function CriaPessoa (nome, sobrenome, idade) {
     return {
         nome: nome,
-        sobrenome: sobrenome,                               // função para criar objeto
+        sobrenome: sobrenome,                              
         idade: idade
     }
 }
 
 const pessoa1 = CriaPessoa('Luiz', 'Otavio', 25)
 console.log(pessoa1)
+
+*/
+
+const pessoa1 = {
+    nome: 'davi',
+    sobrenome: 'vitorino',
+    idade: 31,
+
+    fala() {
+        console.log(`minha idade atual é ${this.idade}.`)
+    },
+
+    incrementarIdade() {
+        this.idade++
+    }
+}
+
+pessoa1.fala()
+pessoa1.incrementarIdade()
+pessoa1.fala()
