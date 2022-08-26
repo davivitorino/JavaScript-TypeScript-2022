3c93397c27b58b5c41d61406742c314c4bce4ff5
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-08-26 19:22:34 -0300

    exercicio atualizado

--- JavaScript Básico/aula32/js/script.js
+++ JavaScript Básico/aula32/js/script.js
@@ -1,12 +1,28 @@
 function meuEscopo () {
     const form = document.querySelector('.form')
+
+    const pessoas = []
     
     function recebeEventoForm (evento) {
-        evento.preventDefault()
-        console.log('nao enviado!')
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        var cadastro = new Object
+            cadastro[0] = nome.value
+            cadastro[1] = sobrenome.value
+            cadastro[2] = peso.value
+            cadastro[3] = altura.value
+        
+        pessoas.push(cadastro)
+        console.log(pessoas)
     }
     
     form.addEventListener('submit', recebeEventoForm)
+
 }
 
 meuEscopo()
