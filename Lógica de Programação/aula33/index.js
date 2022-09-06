c583848fe338e2744ad39a36a78c5d244703c6b1
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-09-06 17:52:26 -0300

    operadores de comparação

---
+++ Lógica de Programação/aula33/index.js
@@ -0,0 +1,13 @@
+/* 
+    Operadores de comparação
+
+    >       maior que
+    >=      maior ou igual que
+    <       menor que
+    <=      menor ou igual que
+    ==      igualdade
+    ===     igualdade estrita
+    !=      diferente
+    !==     diferente estrito
+    
+*/
