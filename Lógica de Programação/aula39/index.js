4f7df3ca3d035077a6cdc59d37a53639f1c45ecd
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-09-15 17:38:09 -0300

    operador ternario

---
+++ Lógica de Programação/aula39/index.js
@@ -0,0 +1,11 @@
+/*operador ternário*/
+
+// ()condição ? 'valor verdadeiro' : 'valor falso';
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'usuário vip' : 'usuário normal';
+
+// definindo valor na variavel
+
+const corUsuario = null;                        // cor usuário 'vazio'
+const corPadrao = corUsuario || 'preta'         // definição do usuário
