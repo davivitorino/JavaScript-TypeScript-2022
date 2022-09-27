295af0755be107aca97d8aea2840ee0c7d97ac2f
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-09-27 07:45:35 -0300

    switch case

---
+++ Lógica de Programação/aula41/index.js
@@ -0,0 +1,33 @@
+const data = new Date();
+let diaSemana = data.getDay();
+let diaSemanaTexto;
+
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda-feira';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça-feira';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta-feira';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta-feira';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta-feira';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabádo';
+        break;
+    default:
+        diaSemanaTexto = '';
+        break;
+    
+}
+
+console.log(diaSemanaTexto)
