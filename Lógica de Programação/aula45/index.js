6ac713579e95b4603a34c78a1603f7aca4f0ba8c
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-05 17:09:14 -0300

    desestruturacao em objeto

---
+++ Lógica de Programação/aula45/index.js
@@ -0,0 +1,24 @@
+// Atribuição via desestruturação em objeto
+
+const pessoa = {
+    nome: 'luiz',
+    sobrenome: 'miranda',
+    idade: 30,
+    endereco: {
+        rua: 'av brasil',
+        numero: 320
+    }
+};
+
+//const { nome } = pessoa;    // criando variavel como mesmo nome da chave
+//console.log(nome)
+
+const { endereco: {rua} } = pessoa; // pegando dado especifico
+console.log(rua)    
+
+const { endereco: {rua: r = 'av japao'}} = pessoa // atribuindo valor padrao caso nao encontre valor no objeto de origem
+console.log(r) // resultado caso nao ache valor : 'av japao'
+
+const { nome, ...resto} = pessoa;   // pegando resto dos dados
+console.log(resto)  // chamando resto
+
