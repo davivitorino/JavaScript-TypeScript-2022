82858daac6e681aa4391d1381722520fd9ae5f90
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-06 18:38:33 -0300

    estrutura de repetição for

---
+++ Lógica de Programação/aula46/index.js
@@ -0,0 +1,29 @@
+// estrutura de repetição 'for' clássico
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`linha ${i}`)
+}
+
+// incrementando de 10 em 10 '+='
+
+for (let i = 0; i <= 10; i+= 10) {
+    console.log(`linha ${i}`)
+}
+
+// decrementa '-='
+
+for (let i = 100; i >= 10; i-= 10) {
+    console.log(`linha ${i}`)
+}
+
+// utilizando em array
+
+const frutas = ['maça', 'uva', 'pêra', 'banana'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`indice ${i}`, frutas[i]);
+}
+
+
+
+
