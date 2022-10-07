8a5ba3606900a8f1561218cf2faaf048ad884cb6
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-07 16:52:18 -0300

    atualizado

--- Lógica de Programação/aula47/assets/js/main.js
+++ Lógica de Programação/aula47/assets/js/main.js
@@ -1,11 +1,10 @@
-let section = document.querySelector(".container");
 
 const elementos = [{tag:"p", texto:'frase1'},{tag:"div", texto:'frase2'}]
 
 for(let i =0; i < elementos.length; i++) {
-    section = document.createElement(elementos[i].tag);
-    section.innerHTML = `${elementos[i].texto}`;
-    document.body.appendChild(section);
+    let element = document.createElement(elementos[i].tag);
+    element.innerHTML = `${elementos[i].texto}`;
+    document.querySelector(".container").appendChild(element);
 }
 
 
