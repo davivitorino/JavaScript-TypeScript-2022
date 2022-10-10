709d51bb8f510c772b7966f1c7ecc8ebe41807b2
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-10 17:12:21 -0300

    estruturas atualizadas

--- Lógica de Programação/aula48/index.js
+++ Lógica de Programação/aula48/index.js
@@ -6,6 +6,8 @@ for (let i in frutas) {
     console.log(frutas[i]);
 }
 
+console.log('') // espaçamento
+
 // acessando chave de objeto
 
 const pessoa = {
@@ -17,3 +19,17 @@ const pessoa = {
 for (let key in pessoa) {
     console.log(key, pessoa[key])
 }
+
+console.log('') // espaçamento
+
+// estrutura de repetição "for of"
+
+for (let valor of frutas ) {
+    console.log(valor)
+}
+
+/**RESUMO**/
+
+// for "classico" : funciona com interáveis (arrays/strings)
+// for in : retorna o indice/chave (string, array, objetos)
+// for of : retorna o valor (interáveis, array, string)
