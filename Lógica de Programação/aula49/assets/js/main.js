8967cd922799d2932e52faef26f2d72cb37ad03e
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-13 01:40:08 -0300

    nodelist e estilo com js

---
+++ Lógica de Programação/aula49/assets/js/main.js
@@ -0,0 +1,13 @@
+// exercicio NodeList, mudando a cor via JavaScript
+
+const paragrafo = document.querySelectorAll('p');
+
+const estilobody = getComputedStyle(document.body); // pega os estilos disponiveis/aplicados ao html 
+const backgroundColorBody = estilobody.backgroundColor; // pegando a propriedade 'backgroundcolor'
+//console.log(backgroundColorBody); chamando no browser
+
+// aplicando as configurações via JS
+for (let p of paragrafo) {
+    p.style.backgroundColor = "#175666";
+    p.style.color = "#ffffff";
+}
