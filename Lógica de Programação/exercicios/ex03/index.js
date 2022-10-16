0d809704bcac71f25ce21b2f7abf019181b8fa00
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-16 11:26:09 -0300

    exercicio

---
+++ Lógica de Programação/exercicios/ex03/index.js
@@ -0,0 +1,18 @@
+// divisor FizzBuzz
+
+// function not be optimized!
+
+
+
+let validNumber = (n) => { return !isNaN(n) ? n : false};
+    
+console.log(validNumber(1))
+
+/*
+if(n % 3 == 0 && n % 5 == 0) {
+    return "FizzBuzz"
+}
+*/
+
+
+
