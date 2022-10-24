14f3677c761067aac7c468b650ee96aefd79ed3e
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-24 15:09:34 -0300

    atualizado

--- Lógica de Programação/aula54/assets/js/main.js
+++ Lógica de Programação/aula54/assets/js/main.js
@@ -1,27 +1,33 @@
-//const relogio = document.querySelector('.relogio');
+const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
-
 
 let seg = 0;
 let min = 0;
 let hora = 0;
 
+relogio.innerHTML += `0${hora}:0${min}:0${seg}`;
+
+
 // iniciando cronometro
 iniciar.addEventListener('click', (event) => {
    
     let timer = setInterval(() => { 
-        seg += 1;
-        let sec = document.querySelector('.relogio')
-        sec.innerHTML = `${seg}`
+        seg += 1;  
+
+        seg < 10 ? relogio.innerHTML = `0${hora}:0${min}:0${seg}` : relogio.innerHTML = `0${hora}:0${min}:${seg}`;
+
+        seg >= 15 ? (min+= 1) (seg = 0) : null ;
+
+        min >= 15 ? (hora+= 1) (seg = 0) (min = 0) : null ;
+
+
     }, 1000);
 
     setTimeout(() => {
         clearInterval(timer);
-    }, 10000);
-
-
+    }, 65000);
 
 });
 
