f2190d6c91edc1df9cca2c83e4423cbfe52ea344
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-10-28 14:53:22 -0300

    exercicio atualizado

--- Lógica de Programação/aula54/assets/js/main.js
+++ Lógica de Programação/aula54/assets/js/main.js
@@ -7,28 +7,38 @@ let seg = 0;
 let min = 0;
 let hora = 0;
 
-relogio.innerHTML += `${hora} ${min} ${seg}`;
+let zero_seg = 0;
+let zero_min = 0;
+let zero_ho = 0;
+
+relogio.innerHTML += `${zero_ho}${hora}:${zero_min}${min}:${zero_seg}${seg}`;
 
 // iniciando cronometro
-iniciar.addEventListener('click', (event) => {
-    
-    
-    let timer = setInterval(() => { 
-       
-        seg += 1;  
-
-        seg > 15 ? (min+= 1) (seg = 0) : null ;
-
-        min > 15 ? (hora+= 1) (seg = 0) (min = 0) : null ;
-
-        relogio.innerHTML = `${hora} ${min} ${seg}`;
+iniciar.addEventListener('click', () => {
+     
+    setInterval(() => { 
         
-    }, 1000);
+        seg += 1;  
+        
+        if(seg > 12) return min += 1, seg = 0;
+        
+        if(min > 12) return hora += 1, seg = 0, min = 0;
+
+        if(hora > 23) return hora = 0, zero_ho = 0;
+        
+        seg > 9 ? zero_seg = '' : zero_seg = 0;
+        min > 9 ? zero_min = '' : zero_min = 0;
+        hora > 9 ? zero_ho = '' : zero_ho = 0;
+        
+        relogio.innerHTML = `${zero_ho}${hora}:${zero_min}${min}:${zero_seg}${seg}`;
+        
+    }, 10);
     
- 
+    /*
     setTimeout(() => {
         clearInterval(timer);
-    }, 65000);
+    }, 2000000);
+    */
 
 });
 
