466113b31396cc7913c791000080b8f02c874cbf
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-11-08 19:07:44 -0300

    atualizado

--- Lógica de Programação/aula55/assets/js/main.js
+++ Lógica de Programação/aula55/assets/js/main.js
@@ -16,15 +16,42 @@ inputTarefa.addEventListener('keypress', function(e) {
     }
 });
 
+// função para limpar campo e voltar foco
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+// criando botão apagar tarefa
+function criaBotaoApagar(li) {
+    li.innerText +=  '  ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+    li.appendChild(botaoApagar);
+}
+
 // função para criar uma nova tarefa
 function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerText = textoInput;
     tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
 };
 
 // evento para iniciar
 btnTarefa.addEventListener('click', function() {
     if(!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
+});
+
+// removendo elemento
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if(el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
 });
