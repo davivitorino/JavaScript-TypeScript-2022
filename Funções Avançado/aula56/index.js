9c6ba7839948518950c3e9483bb58eaa2fb550f1
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-11-10 11:08:29 -0300

    função avançado

---
+++ Funções Avançado/aula56/index.js
@@ -0,0 +1,35 @@
+// Declaração de função (Function hoisting)
+falaOi(); // função é chamada antes da declaração
+
+function falaOi() {
+    console.log('oi');
+}
+
+// First-class objects (Objetos de primeira classe)
+// Function expression
+const MostrarDado = function() {
+    console.log('sou um dado')
+};
+
+//MostrarDado();
+
+// chamando função através de outra função
+function executarFuncao(funcao) {
+    funcao();
+}
+executarFuncao(MostrarDado);
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+};
+funcaoArrow();
+
+// Dentro do objeto
+const obj = {
+    falar() {
+        console.log('Estou falando...');
+    }
+};
+obj.falar();
+
