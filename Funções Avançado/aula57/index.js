0fe3b0b07640239eadb9efc665205f2401c8ecf1
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-11-16 19:43:00 -0300

    atualizado

--- Funções Avançado/aula57/index.js
+++ Funções Avançado/aula57/index.js
@@ -19,3 +19,25 @@ function funcao2(a, b = 2, c = 4) { // valor padrão no paramêtro assume caso n
 }
 
 funcao2(2, undefined, 10) // "undefined" anula valor passado via argumento == 0
+
+// desestruturação literal "Array" ou "objeto"
+
+function funcao3([valor1, valor2, valor3]) {
+    console.log(valor1, valor2, valor3);
+}
+
+funcao3(['nome','idade','profissão']);
+
+// desestruturação via "rest - operation"
+
+function funcao4(operador, acumulador, ...numeros) { // ..."rest" pega o resto do último paramêtro
+    for(let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -= numero;
+        if (operador === '/') acumulador /= numero;
+        if (operador === '*') acumulador *= numero;
+    }
+    console.log(acumulador)
+}
+
+funcao4('+', 10, 20, 30, 40);
