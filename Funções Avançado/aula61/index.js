487124a20a7ec9ed5182429605868161ffbfa1f1
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-12-01 20:00:57 -0300

    iife

---
+++ Funções Avançado/aula61/index.js
@@ -0,0 +1,17 @@
+// IIFE - immediately invoked function expression
+// função invocada imediatamente fora do escopo global
+
+(function(idade, peso, altura) {
+    const sobrenome = 'miranda';
+    function criaNome(nome) {
+        return nome + ' ' + sobrenome;
+    }
+
+    function falaNome() {
+        console.log(criaNome('luiz'));
+    }
+
+    falaNome();
+    console.log(idade, peso, altura);
+
+})(30, 80, 1.80);
