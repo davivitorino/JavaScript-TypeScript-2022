86d14a2989c598cadc5d86588be0a2fa1d8121b0
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-12-04 06:09:49 -0300

    factory function

---
+++ Funções Avançado/aula62/index.js
@@ -0,0 +1,36 @@
+// factory function (função fábrica)
+
+function criaPessoa(nome, sobrenome, altura, peso) {
+    return {
+        nome,
+        sobrenome,
+        altura,
+        peso,
+        get imc() { // Getter converte function em atributo
+            const valorIMC = this.peso / (this.altura ** 2);
+            return valorIMC.toFixed(2);
+        },
+// setter permite adicionar novos atributos
+        set nomeCompleto(dados) { // setter
+            dados = dados.split(' ');
+            this.nome = dados.shift();
+            this.sobrenome = dados.join(' ');
+        },
+
+        fala() {
+            return `${this.nome} ${this.sobrenome} está com imc de: ${this.imc}`
+        }
+    };
+};
+
+const p1 = criaPessoa('davi', 'silva', 1.7, 75);
+
+// chamando imc como atributo e não como função, usando "get"
+// console.log(p1.imc)
+
+// utilizando setter para modificar atributo
+p1.nomeCompleto = 'Maria Oliveira Silva'
+
+const p2 = criaPessoa('joão', 'carlos', 1.8, 90);
+console.log(p1.fala());
+console.log(p2.fala());
