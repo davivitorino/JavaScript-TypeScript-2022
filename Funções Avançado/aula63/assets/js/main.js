1c0cc3a230942c510f4d5426a45325c82361e5c4
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-12-08 09:46:28 -0300

    calculadora factory function

---
+++ Funções Avançado/aula63/assets/js/main.js
@@ -0,0 +1,29 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+        
+        inicia() {
+            this.cliqueBotoes();
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                console.log(this)
+            });
+        },
+
+        btnParaDisplay(valor) {
+          this.display.value += valor;  
+        }
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
