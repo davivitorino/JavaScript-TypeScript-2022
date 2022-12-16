0be2c1d6d27626974ce38c5ff364a9a04399b070
davivitorino <107270876+davivitorino@users.noreply.github.com>
2022-12-16 10:10:10 -0300

    atualizado

--- Funções Avançado/aula63/assets/js/main.js
+++ Funções Avançado/aula63/assets/js/main.js
@@ -1,3 +1,6 @@
+// factory function
+
+/*
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
@@ -75,3 +78,82 @@ function criaCalculadora() {
 
 const calculadora = criaCalculadora();
 calculadora.inicia();
+*/
+
+// constrution fuction
+
+function calculadora() {
+    
+    this.display = document.querySelector('.display');
+
+    this.inicia = () => {
+        this.cliqueBotoes();
+        this.pressionaEnter();
+    }
+
+    this.realizaConta = () => {
+        let conta = this.display.value;
+
+        try {
+            conta = eval(conta);
+
+            if(!conta) {
+                alert('conta inválida');
+                return;
+            }
+
+            this.display.value = String(conta);
+        } catch(e) {
+            alert('conta inválida');
+            return
+        }
+    }
+    
+    this.clearDisplay = () => {
+        this.display.value = '';
+    }
+
+    this.apagaUm = () => {
+        this.display.value = this.display.value.slice(0, -1);
+    }
+
+    this.cliqueBotoes = () => {
+        document.addEventListener('click', (e) => {
+            const el = e.target;
+
+            if(el.classList.contains('btn-num')) {
+                this.btnParaDisplay(el.innerText);
+            }
+
+            if(el.classList.contains('btn-clear')) {
+                this.clearDisplay();
+            }
+
+            if(el.classList.contains('btn-del')) {
+                this.apagaUm();
+            }
+
+            if(el.classList.contains('btn-eq')) {
+                this.realizaConta();
+            }
+
+            this.display.focus();
+        });
+    }
+
+    this.pressionaEnter = () => {
+        this.display.addEventListener('keyup', e => {
+            if(e.keyCode === 13) {
+                this.realizaConta();
+            }
+        });
+    }
+
+    this.btnParaDisplay = (valor) => {
+        this.display.value += valor;  
+    }
+}
+
+const calc = new calculadora()
+
+calc.inicia();
